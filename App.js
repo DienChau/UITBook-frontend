95713423892539ff26f05f2c2469acf561c33b12
yennhi16 <90606980+yennhi16@users.noreply.github.com>
2023-01-03 22:54:42 +0700

    changcolor

--- App.js
+++ App.js
@@ -2,6 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("Hi");
+  const getMoviesFromApiAsync = async () => {
+    try {
+      const response = await fetch("https://reactnative.dev/movies.json");
+      const json = await response.json();
+      console.log("API:", json.movies);
+      return json.movies;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  getMoviesFromApiAsync();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#d592ff",
     alignItems: "center",
     justifyContent: "center",
   },
